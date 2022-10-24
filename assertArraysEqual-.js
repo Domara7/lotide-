4838a61bd244e7b263bf3b726d0327bf6b3c3be3
Domara Hawil <domara.hawil@hotmail.com>
2022-10-24 11:15:44 -0400

    new

--- assertArraysEqual-.js
+++ assertArraysEqual-.js
@@ -1,15 +1,15 @@
-// const eqArrays = function(ar1, ar2){
-//   for (let i = 0; i < ar1.length; i++){
-//     if (ar1[i] === ar2[i]){
-//       return true;
-//      }
-//      if (ar1[i] !== ar2[i]){
-//       return false
-//      }
-//     }
-//   }
+const eqArrays = function(ar1, ar2){
+  for (let i = 0; i < ar1.length; i++){
+    if (ar1[i] === ar2[i]){
+      return true;
+     }
+     if (ar1[i] !== ar2[i]){
+      return false
+     }
+    }
+  }
 
-  const eqArrays = function (ar1, ar2) {
+  const assertArraysEqual = function (ar1, ar2) {
       if (ar1 === ar2) {
          console.log(` TRUE: Array 1 is eqaul to Array 2`);
       }else {
