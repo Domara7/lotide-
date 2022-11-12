54d93772de3fea7eea0361963a06cd3bd74a7039
Domara Hawil <domara.hawil@hotmail.com>
2022-11-12 18:40:48 -0500

    refactored

---
+++ test/eqArraysTest.js
@@ -0,0 +1,20 @@
+const assertEqual = require('../assertEqual');
+
+const eqArrays = function(ar1, ar2){
+  for (let i = 0; i < ar1.length; i++){
+    if (ar1[i] === ar2[i]){
+      return true;
+     }
+     if (ar1[i] !== ar2[i]){
+      return false
+     }
+    }
+  }
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+
+
+
+
