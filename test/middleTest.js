3ae6970e933251f633ca32eac7a5f31b9db03d4a
Domara Hawil <domara.hawil@hotmail.com>
2022-11-14 17:35:56 -0500

    new

--- test/middleTest.js
+++ test/middleTest.js
@@ -3,6 +3,6 @@ const middle = require('../middle');
 
 describe("#middle", () => {
   it("returns 2 for [1, 2, 3]", () => {
-    assert.strictEqual(middle([1, 2, 3]), 2);
+    assert.deepEqual(middle([1, 2, 3]), [2]);
   });
 });
