f048379463c9d3826ac5ee6d64322cc4b7abba1b
Domara Hawil <domara.hawil@hotmail.com>
2022-11-22 10:56:44 -0500

    fixes

--- test/assertEqualtest.js
+++ test/assertEqualtest.js
@@ -1,25 +1,9 @@
-// const assertEqual = function (actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-
-// };
-
-
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("bob", "bob");
-
-// module.exports = assertEqual;
-
 const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
 describe("#assertEqual", () => {
   it("returns true if the arrays are equal", () => {
-    assert.deepEqual(assertEqual([1, 2, 3] === [1, 2, 3]), true);
+    assert.deepEqual(assertEqual([1, 2, 3], [1, 2, 3], true));
   });
 });
 
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,25 +1,9 @@
-// const assertEqual = require('../assertEqual');
-
-// const eqArrays = function(ar1, ar2){
-//   for (let i = 0; i < ar1.length; i++){
-//     if (ar1[i] === ar2[i]){
-//       return true;
-//      }
-//      if (ar1[i] !== ar2[i]){
-//       return false
-//      }
-//     }
-//   }
-
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
 const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
 describe("#eqArrays", () => {
-  it("returns true for [1, 2, 3] === [1, 2, 3]", () => {
-    assert.deepEqual(eqArrays([1, 2, 3]),[1, 2, 3]);
+  it("returns true if the ", () => {
+    assert.deepEqual(eqArrays([1, 2, 3],[1, 2, 3]), true);
   });
 });
 
